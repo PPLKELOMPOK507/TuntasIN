23777dbeec89a93eed41eca38c57e33b678230b3
ardeliagatha <ardeliagatha@gmail.com>
2025-04-14 22:36:08 +0700

    update

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static ::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
@@ -35,7 +35,7 @@ class UserFactory extends Factory
     /**
      * Indicate that the model's email address should be unverified.
      */
-    public function unverified(): static
+    public function unverified():static
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
