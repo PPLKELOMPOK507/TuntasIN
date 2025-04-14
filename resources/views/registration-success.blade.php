22940ce3835a4ddaa56ce549391440940f24f3d7
MeizonRura <meizonruraarung27@gmail.com>
2025-04-14 23:46:28 +0700

    TNTSIN-17 Dashboard PBI#5

--- resources/views/registration-success.blade.php
+++ resources/views/registration-success.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="success-container">
     <h1>Registration Successful!</h1>
-    <p>Welcome to TuntasIN.</p>
+    <p>Welcome to TuntasIN.ID</p>
     <a href="/" class="back-home"> Back to Dashboard</a>
-    
+
 </div>
 @endsection
