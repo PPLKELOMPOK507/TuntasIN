ebb418f7a1514971a18c435d710619cee6ff0d73
jelitapriskila <jelita.net0428@gmail.com>
2025-05-21 17:14:11 +0700

    TNTSIN-57
    ganti method

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,6 +72,7 @@
     @stack('styles')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+@stack('scripts')
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <body>
     @yield('content')
